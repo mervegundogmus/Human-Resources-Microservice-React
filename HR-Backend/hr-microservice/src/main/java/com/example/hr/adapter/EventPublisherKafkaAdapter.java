38711d1a7e5ea8e0136531ab29a0a844b62438a8
Merve Gündoğmuş <68029482+mervegundogmus@users.noreply.github.com>
2023-03-25 14:28:57 +0300

    Update EventPublisherKafkaAdapter.java

--- HR-Backend/hr-microservice/src/main/java/com/example/hr/adapter/EventPublisherKafkaAdapter.java
+++ HR-Backend/hr-microservice/src/main/java/com/example/hr/adapter/EventPublisherKafkaAdapter.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-
 import com.example.hr.application.business.events.EmployeeEvent;
 import com.example.hr.infrastructure.EventPublisher;
 import com.fasterxml.jackson.core.JsonProcessingException;
