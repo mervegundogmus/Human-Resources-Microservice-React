afdfb1b7bf9eac5220f7344417b0fae6d65ecac6
Merve Gündoğmuş <68029482+mervegundogmus@users.noreply.github.com>
2023-03-25 14:34:36 +0300

    Update HireEmployeeRequest.java

--- HR-Backend/hr-microservice/src/main/java/com/example/hr/dto/request/HireEmployeeRequest.java
+++ HR-Backend/hr-microservice/src/main/java/com/example/hr/dto/request/HireEmployeeRequest.java
@@ -1,7 +1,6 @@
 package com.example.hr.dto.request;
 
 import java.util.List;
-
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
