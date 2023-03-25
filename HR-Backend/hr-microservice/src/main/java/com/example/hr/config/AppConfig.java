d362d5d5c61989d956fb39a92c8b49bf1e9b2f7b
Merve Gündoğmuş <68029482+mervegundogmus@users.noreply.github.com>
2023-03-25 14:29:29 +0300

    Update AppConfig.java

--- HR-Backend/hr-microservice/src/main/java/com/example/hr/config/AppConfig.java
+++ HR-Backend/hr-microservice/src/main/java/com/example/hr/config/AppConfig.java
@@ -2,7 +2,6 @@ package com.example.hr.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import com.example.hr.application.HrApplication;
 import com.example.hr.application.business.StandardHrApplication;
 import com.example.hr.infrastructure.EventPublisher;
